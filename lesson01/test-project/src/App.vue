6023d4acdd17584200b547faef8647a88f77a97e
AlexShep5 <alxshep25@gmail.com>
2022-10-19 19:38:33 +0300

    lesson01

---
+++ lesson01/test-project/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <input class="inputField" type="number" v-model="operand1">
+        <input class="inputField" type="number" v-model="operand2">
+
+        <p>Результат: {{ result }}</p>
+
+        <div>
+            <button class="calcBtn" v-on:click="result = Number(operand1) + Number(operand2)">+</button>
+            <button class="calcBtn" v-on:click="result = operand1 - operand2">-</button>
+            <button class="calcBtn" v-on:click="result = operand1 * operand2">*</button>
+            <button class="calcBtn" v-on:click="result = operand1 / operand2">/</button>
+        </div>
+        <div>
+            <button class="calcBtn" v-on:click="result = Math.pow(operand1, operand2)">^</button>
+            <button class="calcBtn" v-on:click="result = operand1 % operand2">%</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            operand1: 0,
+            operand2: 0,
+            result: 0
+        }
+    }
+}
+
+</script>
+
+<style lang="scss">
+.inputField {
+    width: 70px;
+    &:not(:first-child) {
+        margin-left: 8px;
+    }
+}
+.calcBtn {
+    margin: 4px;
+    width: 32px;
+    height: 32px;
+}
+
+</style>
